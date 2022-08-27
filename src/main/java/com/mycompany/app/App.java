f3d7c22f4a12c2d89c2bd7e084b3e5ae9cdf9456
vaishakhs <111145609+vaishakhs@users.noreply.github.com>
2022-08-27 15:53:35 +0530

    Second commit

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World! From Anavattii-577413" );
+        System.out.println( "Hello World! From Anavattii-577413 Karnataka State India IN" );
     }
 }
